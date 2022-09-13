1cbed34054f267c71a9f9827b177e9494c20d7a6
Bohdan Mukha <by.muha@gmail.com>
2022-09-13 22:31:22 +0300

    default import works

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-alert('it works!');
+import { defaultResults } from './defaultResults.js';
+
+// const fetchPeople = async () => {
+//   try {
+//     const response = await fetch(
+//       'https://randomuser.me/api/?results=50&inc=gender,name,phone,age,email,picture&noinfo'
+//     );
+//     const data = await response.json();
+//     console.log(JSON.stringify(data));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// fetchPeople();
+console.log(defaultResults.results);
